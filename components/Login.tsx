a3f3f71c5916614c4a6cc5175c4756bc67c57c93
Ayush kumar Singh <ayusharon209@gmail.com>
2025-05-15 15:54:41 +0530

    Login and signup page

---
+++ components/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100'>
+      <main className='flex flex-col itens-center justify-center w-full flex-1 px-20 text-center'>
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+          <div> <p>Sign In section</p></div>
+          <div> <p>Sign Up section</p></div>
+
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Login
